4e97d285b5baf15860261877ed3f57e58bb48dc9
Pedro Roure Malta de Sa <pedrorouremalta@gmail.com>
2022-05-30 22:53:38 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,122 @@
+# f5-awaf-policy-suggestions-merger
+
+This is a small script which can be used to automate/facilitate the process of building an ASM security policy.
+
+The script leverages the BIG-IP **iControl REST API** and the **Declarative WAF REST API** to facilitate the process of merging an ASM policy with all its learning suggestions in an attempt the generate a more stable policy (which will result in less false positives when put in production). 
+
+This tool must be used only when all traffic reaching your ASM security policy is valid/trusted. Some scenarios include:
+
+    1. Automated tests in a CI/CD pipeline.
+    2. Manual tests by a team (e.g QA team).
+
+In both scenarios is a **MUST-HAVE** that all traffic reaching the application (and ASM security policy) is valid traffic (non-attack traffic). 
+
+## Usage
+
+```
+usage: f5-awaf-policy-suggestions-merger.py [-h] --device DEVICE --username USERNAME --password PASSWORD --policy POLICY [--action {only-save-suggestions,only-save-policy,merge-save,merge-save-import,merge-import}] [--suggestionsfile SUGGESTIONSFILE]
+                                            [--policyfile POLICYFILE] [--dirpath DIRPATH]
+
+A small script to export a WAF policy and all its suggestions, merge them and then import the policy again
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --device DEVICE
+  --username USERNAME
+  --password PASSWORD
+  --policy POLICY
+  --action {only-save-suggestions,only-save-policy,merge-save,merge-save-import,merge-import}
+  --suggestionsfile SUGGESTIONSFILE
+  --policyfile POLICYFILE
+  --dirpath DIRPATH, -o DIRPATH
+  ```
+
+## Options
+
+| Argument | Description | Required | Default |
+|----------|-------------|----------|---------|
+| --device | BIG-IP IP address | Yes | No |
+| --username | Admin username for BIG-IP | Yes | No | 
+| --password | Admin password for BIG-IP | Yes | No | 
+| --policy | ASM policy name (full path: /\<partition>/\<policy name>). | Yes | No |
+| --action | The **operation mode** of the script.| No | merge-save |
+| --suggestionsfile | Specify a *suggestions file* to be used instead of retrieve the learning suggestions from BIG-IP. You use this option when you want to customize the suggestions before merging them with ASM policy. | No | No |
+| --policyfile | Specify a *ASM policy file* to be used instead of retrieve the ASM policy from BIG-IP. You use this option when you want to customize the ASM policy before merging with its learning suggestions. | No | No |
+
+### Operation modes
+
+| Mode | Description | Use case |
+|------|-------------|----------|
+| only-save-suggestions | This mode will only download the *learning suggestions* in a JSON format and save them in current directory or in a directory specified by the option *--dirpath*.| You use this option when you want to customize the *learning suggestions* before the merging with the ASM policy.|
+| only-save-policy | This mode will only download the *ASM policy* in a JSON format and save them in current directory or in a directory specified by the option *--dirpath*.| You use this option when you want to customize the *ASM policy* before the merging with its *learning suggestions*.|
+| merge-save | This mode will export the *ASM policy* (or use the ASM policy specified by the option *--policyfile*), export the *learning suggestions* (or use the learning suggestions file specified by the option *--suggestionsfile*), and then merge them. The three following files will be save in the current directory or in the directory specified by the option *--dirpath*: a *ASM policy* file (extension *.policy.json*), a *learning suggestions* file (extension *.suggestions.json*) and a *merged policy* file (extension *.mergedpolicy.json*).| This is a non-intrusive mode that you can use to check the *ASM merged policy* before the actual import.|
+| merge-save-import | Do the same that the operation mode **merge-save** but with a aditional step of import the *ASM merged policy* in the BIG-IP | Use this option when you sure that you import the *ASM merged policy* in the BIG-IP. | 
+| merge-import | Do the same that the operation mode **merge-save-import** but without saving the files in the local system.| Use this option when you want to merge the current ASM policy with all its suggestions without saving them in the local filesystem.| 
+
+## Scenarios
+
+### Scenario 1 (fully automated)
+
+The script will export the ASM policy, export the learning suggestions, and then merge them. The ASM merged policy will be upload, imported and applied on the BIG-IP.
+
+```
+python f5-awaf-policy-suggestions-merger.py --device "X.X.X.X" --username "admin" --password "XXXXXXX" --policy "/Common/asmpolicy_dvwa" --action merge-import
+```
+
+Alternatively you can use the action **merge-save-import** to have the *ASM policy* file, *learning suggestions* file and *ASM merged policy* file save on the current directory or in a directory specified by the option *--dirpath*.
+
+### Scenario 2 (semi automated, customized **ASM policy file**)
+
+This scenario is comprised of three steps:
+
+1. Download the **ASM policy file**:
+
+```
+python f5-awaf-policy-suggestions-merger.py --device "X.X.X.X" --username "admin" --password "XXXXXXX" --policy "/Common/asmpolicy_dvwa" --action only-save-policy --dirpath tmp
+```
+
+2. Customize the **ASM policy file** (e.g. change the enforcement mode).
+
+3. Export the learning suggestions for the policy and merge them with local (customized) **ASM policy file**. The ASM merged policy will be upload, imported and applied on the BIG-IP.
+
+```
+python f5-awaf-policy-suggestions-merger.py --device "X.X.X.X" --username "admin" --password "XXXXXXX" --policy "/Common/asmpolicy_dvwa" --action merge-import --policyfile tmp/Common-asmpolicy_dvwa.policy.json
+```
+
+### Scenario 3 (semi automated, customized **learning suggestions file**)
+
+1. Download the **learning suggestions file**:
+
+```
+python f5-awaf-policy-suggestions-merger.py --device "X.X.X.X" --username "admin" --password "XXXXXXX" --policy "/Common/asmpolicy_dvwa" --action only-save-suggestions --dirpath tmp
+```
+
+2. Customize the **learning suggestions file** (e.g. remove learning suggestions that you are not sure about them).
+
+3. Export the ASM policy and merge it with the local (customized) **learning suggestion file**:
+
+```
+python f5-awaf-policy-suggestions-merger.py --device "X.X.X.X" --username "admin" --password "XXXXXXX" --policy "/Common/asmpolicy_dvwa" --action merge-import --suggestionsfile tmp/Common-asmpolicy_dvwa.suggestions.json
+```
+
+### Scenario 4 (semi automated, customized **ASM policy file** and **learning suggestions file**)
+
+1. Download the **ASM policy file**:
+
+```
+python f5-awaf-policy-suggestions-merger.py --device "X.X.X.X" --username "admin" --password "XXXXXXX" --policy "/Common/asmpolicy_dvwa" --action only-save-policy --dirpath tmp
+```
+
+2. Download the **learning suggestions file**:
+
+```
+python f5-awaf-policy-suggestions-merger.py --device "X.X.X.X" --username "admin" --password "XXXXXXX" --policy "/Common/asmpolicy_dvwa" --action only-save-suggestions --dirpath tmp
+```
+
+3. Customize the **ASM policy file** (e.g. change the enforcement mode) and the **learning suggestions file** (e.g. remove learning suggestions that you are not sure about them).
+
+4. Merge the local (customized) **ASM policy file** with the local (customized) **learning suggestions file**. The ASM merged policy will be upload, imported and applied on the BIG-IP.
+
+```
+python f5-awaf-policy-suggestions-merger.py --device "X.X.X.X" --username "admin" --password "XXXXXXX" --policy "/Common/asmpolicy_dvwa" --action merge-import --suggestionsfile tmp/Common-asmpolicy_dvwa.suggestions.json --policyfile tmp/Common-asmpolicy_dvwa.policy.json
+```
---
+++ f5-awaf-policy-suggestions-merger.py
@@ -0,0 +1,388 @@
+import requests 
+import urllib3
+import json
+import re
+import argparse
+import sys
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class F5PolicySuggestionsMergerException(Exception):
+    pass
+
+class ASM:
+
+    __device = None
+    __policyId = None
+    __session = None
+    __policy_json = None
+    __suggestions_json = None
+    __merged_policy_json = None
+
+    def __init__(self,device,username,password):
+
+        self.__device = device
+
+        self.__session = requests.Session()
+        self.__session.verify=False
+        self.__session.auth = (username,password)
+
+    def __asm_get_policyid_by_name(self,policyName):
+        
+        url = "https://%s/mgmt/tm/asm/policies?$select=name,id,fullPath" % self.__device
+
+        try:
+            resp = self.__session.get(url)
+        except requests.exceptions.RequestException as error:
+                raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+        policies = resp.json()['items']
+
+        for policy in policies:
+
+            if policy['fullPath'] == policyName:
+                self.__policyId = policy['id'] 
+                break
+
+        if self.__policyId == None:
+            raise F5PolicySuggestionsMergerException("(POLICY): No such WAF policy '%s'. Specify a valid WAF policy using the method 'set_policy'." % policyName)
+
+    def set_policy(self,policyName):
+
+        self.__policyName = policyName
+
+        self.__asm_get_policyid_by_name(policyName)
+
+    def export_policy(self):
+
+        if self.__policyId == None:
+            raise F5PolicySuggestionsMergerException("(POLICY): Specify a valid WAF policy using the method 'set_policy'.")
+        
+        filename = self.__policyName[1:].replace("/","-") + "." + "json"
+
+        data = {}
+        data['filename'] = filename
+        data['format'] = "json"
+        data['minimal'] = True
+        data['policyReference'] = {}
+        data['policyReference']['link'] = "https://localhost/mgmt/tm/asm/policies/%s" % (self.__policyId)
+
+        url = "https://%s/mgmt/tm/asm/tasks/export-policy" % self.__device
+
+        try: 
+            resp = self.__session.post(url, json=data)
+        except requests.exceptions.RequestException as error:
+            raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+        taskLink = resp.json()['selfLink'].replace("localhost",self.__device)
+
+        status = 0
+        while True:
+
+            try:
+                resp = self.__session.get(taskLink)
+            except requests.exceptions.RequestException as error:
+                raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+            task_status = resp.json()['status']
+
+            if task_status == "COMPLETED":
+                status = 1
+                break
+        try:
+            resp = self.__session.get("https://%s/mgmt/tm/asm/file-transfer/downloads/%s" % (self.__device,filename))
+        except requests.exceptions.RequestException as error:
+            raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+        self.__policy_json = resp.json()
+
+    def export_suggestions(self):
+
+        if self.__policyId == None:
+            raise F5PolicySuggestionsMergerException("(POLICY): Specify a valid WAF policy using the method 'set_policy'.")
+        
+        filename = self.__policyName[1:].replace("/","-") + "." + "suggestions" + "." + "json"
+
+        data = {}
+        data['filename'] = filename
+        data['format'] = "json"
+        data['policyReference'] = {}
+        data['policyReference']['link'] = "https://localhost/mgmt/tm/asm/policies/%s" % (self.__policyId)
+        
+        url = "https://%s/mgmt/tm/asm/tasks/export-suggestions/" % self.__device
+
+        try:
+            resp = self.__session.post(url, json=data)
+        except requests.exceptions.RequestException as error:
+            raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+        taskLink = resp.json()['selfLink'].replace("localhost",self.__device)
+
+        status = 0
+        while True:
+            
+            try:
+                resp = self.__session.get(taskLink)
+            except requests.exceptions.RequestException as error:
+                raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+            task_status = resp.json()['status']
+
+            if task_status == "COMPLETED":
+                status = 1
+                break
+
+        try:
+            resp = self.__session.get("https://%s/mgmt/tm/asm/file-transfer/downloads/%s" % (self.__device,filename))
+        except requests.exceptions.RequestException as error:
+                raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+        self.__suggestions_json = resp.json()
+
+    def load_suggestions(self,filename):
+
+        try:
+            with open(filename) as file:
+                self.__suggestions_json = json.load(file)
+        except OSError as error:
+            raise F5PolicySuggestionsMergerException("(FILE): %s" % error)
+
+    def load_policy(self,filename):
+
+        try:
+            with open(filename) as file:
+                self.__policy_json = json.load(file)
+        except OSError as error:
+            raise F5PolicySuggestionsMergerException("(FILE): %s" % error)
+
+    def merge_policy_suggestions(self):
+
+        if self.__policyId == None:
+            raise F5PolicySuggestionsMergerException("(POLICY): Specify a valid WAF policy using the method 'set_policy'.")
+
+        self.__merged_policy_json = json.loads(json.dumps(self.__policy_json))
+
+        self.__merged_policy_json['modifications'] = self.__suggestions_json
+
+    def import_policy_merged(self):
+    
+        if self.__policyId == None:
+            raise F5PolicySuggestionsMergerException("(POLICY): Specify a valid WAF policy using the method 'set_policy'.")
+
+        filename = self.__policyName[1:].replace("/","-") + "." + "json"
+
+        url = "https://%s/mgmt/tm/asm/file-transfer/uploads/%s" % (self.__device,filename)
+
+        headers = {"Content-Range": "0-99/100"}
+
+        try:
+            resp = self.__session.post(url, json=self.__merged_policy_json, headers=headers)
+        except requests.exceptions.RequestException as error:
+            raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+        message = resp.json()['message']
+
+        m = re.search('buffer length (.*)',message)
+
+        size = int(m.group(1))
+        
+        headers = {"Content-Range": "0-%d/%d" % (size-1,size)}
+
+        try:
+            resp = self.__session.post(url, json=self.__merged_policy_json,headers=headers)
+        except requests.exceptions.RequestException as error:
+            raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+        url = "https://%s/mgmt/tm/asm/tasks/import-policy/" % (self.__device)
+
+        data = {}
+        data['filename'] = filename
+        data['policy'] = {}
+        data['policy']['fullPath'] = self.__policyName
+
+        resp = self.__session.post(url, json=data)
+
+        # apply the policy which was imported
+        url = "https://%s/mgmt/tm/asm/tasks/apply-policy/" % (self.__device)
+
+        data = {}
+        data['policy'] = {}
+        data['policy']['fullPath'] = self.__policyName
+
+        try:
+            resp = self.__session.post(url, json=data)
+        except requests.exceptions.RequestException as error:
+                raise F5PolicySuggestionsMergerException("(REQUESTS): FAILED %s" % error)
+
+    def save_policy(self,dirpath):
+        
+        if self.__policyId == None:
+            raise F5PolicySuggestionsMergerException("(POLICY): Specify a valid WAF policy using the method 'set_policy'.")
+
+        filename = self.__policyName[1:].replace("/","-") + "." + "policy" + "." + "json"
+
+        try: 
+            with open("%s/%s" % (dirpath,filename), "w") as policy:
+                policy.write(json.dumps(self.__policy_json, indent=2, sort_keys=False))
+        except OSError as error:
+            raise F5PolicySuggestionsMergerException("(FILE): %s" % error)
+
+    def save_suggestions(self,dirpath):
+
+        if self.__policyId == None:
+            raise F5PolicySuggestionsMergerException("(POLICY): Specify a valid WAF policy using the method 'set_policy'.")
+
+        filename = self.__policyName[1:].replace("/","-") + "." + "suggestions" + "." + "json"
+
+        try:
+            with open("%s/%s" % (dirpath,filename), "w") as suggestions:
+                suggestions.write(json.dumps(self.__suggestions_json, indent=2, sort_keys=False))
+        except OSError as error:
+            raise F5PolicySuggestionsMergerException("(FILE): %s" % error)
+
+    def save_merged_policy(self,dirpath):
+
+        if self.__policyId == None:
+            raise F5PolicySuggestionsMergerException("(POLICY): Specify a valid WAF policy using the method 'set_policy'.")
+
+        filename = self.__policyName[1:].replace("/","-") + "." + "policymerged" + "." + "json"
+
+        try:
+            with open("%s/%s" % (dirpath,filename), "w") as mergedPolicy:
+                mergedPolicy.write(json.dumps(self.__merged_policy_json, indent=2, sort_keys=False))
+        except OSError as error:
+            raise F5PolicySuggestionsMergerException("(FILE): %s" % error)
+
+def main():
+    
+    parser = argparse.ArgumentParser(description = 'A small script to export a WAF policy and all its suggestions, merge them and then import the policy again')
+
+    parser.add_argument('--device', type=str, required=True)
+    parser.add_argument('--username', type=str, required=True)
+    parser.add_argument('--password', type=str, required=True)
+    parser.add_argument('--policy', type=str, required=True)
+    parser.add_argument('--action', type=str, required=False, default="merge-save", choices=['only-save-suggestions', 'only-save-policy', 'merge-save','merge-save-import','merge-import'])
+    parser.add_argument('--suggestionsfile', type=str, required=False, default="nofile")
+    parser.add_argument('--policyfile', type=str, required=False, default="nofile")
+    parser.add_argument('--dirpath', '-o', type=str, required=False, default=".")
+
+    args = parser.parse_args()
+
+    device = args.device
+    username = args.username
+    password = args.password
+    policy = args.policy
+    action = args.action
+    suggestionsfile = args.suggestionsfile
+    policyfile = args.policyfile
+    dirpath = args.dirpath
+
+
+    asm = ASM(device,username,password)
+
+    if action == "only-save-suggestions":
+
+        try:
+            asm.set_policy(policy)
+            asm.export_suggestions()
+            asm.save_suggestions(dirpath)
+
+        except F5PolicySuggestionsMergerException as error:
+            print("ERROR %s" % error, file=sys.stderr)
+            sys.exit(1)
+
+        sys.exit(0)
+
+    if action == "only-save-policy":
+
+        try:
+            asm.set_policy(policy)
+            asm.export_policy()
+            asm.save_policy(dirpath)
+
+        except F5PolicySuggestionsMergerException as error:
+            print("ERROR %s" % error, file=sys.stderr)
+            sys.exit(1)
+
+        sys.exit(0)
+
+
+    if action == "merge-save":
+
+        try:
+            asm.set_policy(policy)
+
+            if policyfile == "nofile":
+                asm.export_policy()
+            else:
+                asm.load_policy(policyfile)
+
+            if suggestionsfile == "nofile":
+                asm.export_suggestions()
+            else:
+                asm.load_suggestions(suggestionsfile)
+
+            asm.merge_policy_suggestions()
+            asm.save_policy(dirpath)
+            asm.save_suggestions(dirpath)
+            asm.save_merged_policy(dirpath)
+        
+        except F5PolicySuggestionsMergerException as error:
+            print("ERROR %s" % error, file=sys.stderr)
+            sys.exit(1)
+
+        sys.exit(0)
+
+    if action == "merge-save-import":
+
+        try:
+            asm.set_policy(policy)
+
+            if policyfile == "nofile":
+                asm.export_policy()
+            else:
+                asm.load_policy(policyfile)
+
+            if suggestionsfile == "nofile":
+                asm.export_suggestions()
+            else:
+                asm.load_suggestions(suggestionsfile)
+
+            asm.merge_policy_suggestions()
+            asm.save_policy(dirpath)
+            asm.save_suggestions(dirpath)
+            asm.save_merged_policy(dirpath)
+            asm.import_policy_merged()
+
+        except F5PolicySuggestionsMergerException as error:
+            print("ERROR %s" % error, file=sys.stderr)
+            sys.exit(1)
+
+        sys.exit(0)
+
+    if action == "merge-import":
+
+        try:
+            asm.set_policy(policy)
+
+            if policyfile == "nofile":
+                asm.export_policy()
+            else:
+                asm.load_policy(policyfile)
+
+            if suggestionsfile == "nofile":
+                asm.export_suggestions()
+            else:
+                asm.load_suggestions(suggestionsfile)
+
+            asm.merge_policy_suggestions()
+            asm.import_policy_merged()
+
+        except F5PolicySuggestionsMergerException as error:
+            print("ERROR %s" % error, file=sys.stderr)
+            sys.exit(1)
+
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
